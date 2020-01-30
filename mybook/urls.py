f421b2438ba707777295d6f7f1b6df5c15941306
dede_20191130 <1044adad@gmail.com>
2020-01-30 23:14:16 +0900

    20200130_2314__ .../8effbf6f-a753-4f9d-8b69-c923826a9b4c.xml       | 262 +++++++++++++--------  mybook/__pycache__/forms.cpython-38.pyc            | Bin 0 -> 633 bytes  mybook/__pycache__/urls.cpython-38.pyc             | Bin 471 -> 489 bytes  mybook/__pycache__/views.cpython-38.pyc            | Bin 651 -> 1238 bytes  mybook/forms.py                                    |  11 +  mybook/urls.py                                     |   1 +  mybook/views.py                                    |  33 ++-  templates/mybook/base.html                         |  21 ++  templates/mybook/book_edit.html                    |  22 ++  templates/mybook/book_list.html                    |  33 +++  10 files changed, 286 insertions(+), 97 deletions(-)

--- mybook/urls.py
+++ mybook/urls.py
@@ -5,6 +5,7 @@ from mybook import views
 app_name = 'mybook'
 urlpatterns = [
     # 書籍
+    path('', views.book_list, name='book_list'),  # 一覧
     path('book/', views.book_list, name='book_list'),  # 一覧
     path('book/add/', views.book_edit, name='book_add'),  # 登録
     path('book/mod/<int:book_id>/', views.book_edit, name='book_mod'),  # 修正
